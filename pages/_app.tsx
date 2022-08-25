8e5926069b9e772bb5f99117c4f08ab3f54935f0
sudoak <sudoakk@gmail.com>
2022-08-25 13:46:41 +0530

    feat: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
+/* eslint-disable @next/next/inline-script-id */
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-8XLLMKN8B7"
+      ></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8XLLMKN8B7');`}
+      </Script>
       <Component {...pageProps} />
     </ChakraProvider>
   );
